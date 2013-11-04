bd26c292ac98de74b6601c0a081604c1dfddf17e
MoistyMoist <mr.lawrance@outlook.com>
2013-11-05 00:17:45 +0800

    added some controller and view

--- PetSociety-for-Windows/PetSociety-for-Windows/Src/Controller/LoginController.cs
+++ PetSociety-for-Windows/PetSociety-for-Windows/Src/Controller/LoginController.cs
@@ -2,11 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using PetSociety_for_Windows.Src.Model;
 
 namespace PetSociety_for_Windows.Src.Controller
 {
-    class UserController
+    class LoginController
     {
-  
     }
 }
