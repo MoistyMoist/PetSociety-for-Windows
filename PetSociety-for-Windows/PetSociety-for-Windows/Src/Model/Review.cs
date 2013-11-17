d29a556dc3872965e11db9d141f7359264b66c89
MoistyMoist <mr.lawrance@outlook.com>
2013-11-17 16:17:24 +0800

    dsa

--- PetSociety-for-Windows/PetSociety-for-Windows/Src/Model/Review.cs
+++ PetSociety-for-Windows/PetSociety-for-Windows/Src/Model/Review.cs
@@ -2,10 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using PetSociety_for_Windows.Src.Model;
+
 
 namespace PetSociety_for_Windows.Src.Model
 {
     class Review
     {
+
+         //===============================================================//
+        //                              Attributes
+        //===============================================================//
+
+        private int reviewID;
+        private string description;
+        private string title;
+        private string likes;
+        private string diskiles;
+        private string dateCreated;
+        private string timeCreated;
+
+        private int locationID;
+
+        private User user;
+        private Stray stray;
+
+        //===============================================================//
+        //                            Constructors
+        //===============================================================//
+
+        public Review()
+        {
+
+        }
+
+        //===============================================================//
+        //                              Accessors
+        //===============================================================//
     }
 }
