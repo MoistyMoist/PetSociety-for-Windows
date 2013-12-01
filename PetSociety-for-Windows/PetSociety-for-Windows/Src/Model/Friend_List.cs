5c4891d18b31a74e6ac28aa235d7b8c8451b2943
MoistyMoist <mr.lawrance@outlook.com>
2013-12-01 20:33:55 +0800

    added class

--- PetSociety-for-Windows/PetSociety-for-Windows/Src/Model/Friend_List.cs
+++ PetSociety-for-Windows/PetSociety-for-Windows/Src/Model/Friend_List.cs
@@ -7,5 +7,45 @@ namespace PetSociety_for_Windows.Src.Model
 {
     class Friend_List
     {
+        //===============================================================//
+        //                              Attributes
+        //===============================================================//
+
+        private int friend_listID;
+        private User user;
+        private User friend;
+       
+
+        //===============================================================//
+        //                           Constructors
+        //===============================================================//
+
+        public Friend_List()
+        {
+        }
+
+        //===============================================================//
+        //                              Accessors
+        //===============================================================//
+
+        public int Friend_listID
+        {
+            get { return friend_listID; }
+            set { friend_listID = value; }
+        }
+        internal User User
+        {
+            get { return user; }
+            set { user = value; }
+        }
+        internal User Friend
+        {
+            get { return friend; }
+            set { friend = value; }
+        }
+
+        //===============================================================//
+        //                              End
+        //===============================================================//
     }
 }
