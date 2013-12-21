ad0d265a05a2fa56c724117bff35032789d7e228
MoistyMoist <mr.lawrance@outlook.coml.com>
2013-12-21 13:11:36 +0800

    logout done

--- PetSociety-for-Windows/PetSociety-for-Windows/Map.xaml.cs
+++ PetSociety-for-Windows/PetSociety-for-Windows/Map.xaml.cs
@@ -177,9 +177,9 @@ namespace PetSociety_for_Windows.Pages
             ApplicationBar = new ApplicationBar();
 
             // Create a new button and set the text value to the localized string from AppResources.
-            ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
- //           appBarButton.Text = AppResources.AppBarButtonText;
- //           ApplicationBar.Buttons.Add(appBarButton);
+            ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Resources/Dark/feature.camera.png", UriKind.Relative));
+            appBarButton.Text = "Report";
+            ApplicationBar.Buttons.Add(appBarButton);
 
             // Create a new menu item with the localized string from AppResources.
   //          ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
