303d2734eb78729b0a3106f777340f2346faf82c
MoistyMoist <mr.lawrance@outlook.coml.com>
2013-12-22 13:07:53 +0800

    dsa

--- PetSociety-for-Windows/PetSociety-for-Windows/Map.xaml.cs
+++ PetSociety-for-Windows/PetSociety-for-Windows/Map.xaml.cs
@@ -178,7 +178,7 @@ namespace PetSociety_for_Windows.Pages
             appBarButton.Text = "Add Location";
             ApplicationBar.Buttons.Add(appBarButton);
         }
-        protected override void PhoneApplicationPage_BackKeyPress(object sender, System.ComponentModel.CancelEventArgs e)
+        private void PhoneApplicationPage_BackKeyPress(object sender, System.ComponentModel.CancelEventArgs e)
         {
             StaticObjects.CurrentUser = null;
             AppLifetimeHelper close = new AppLifetimeHelper();
