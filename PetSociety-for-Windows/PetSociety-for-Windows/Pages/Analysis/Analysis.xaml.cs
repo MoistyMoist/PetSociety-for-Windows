4000e188dd09eed05803975bf46fa19bebe3d61d
MoistyMoist <mr.lawrance@outlook.coml.com>
2013-12-25 03:35:57 +0800

    ds

--- PetSociety-for-Windows/PetSociety-for-Windows/Pages/Analysis/Analysis.xaml.cs
+++ PetSociety-for-Windows/PetSociety-for-Windows/Pages/Analysis/Analysis.xaml.cs
@@ -252,5 +252,9 @@ namespace PetSociety_for_Windows.Pages.Analysis
                 }
             }
         }
+        public static double GetDistanceTo(this Point p1, Point p2)
+        {
+            return Math.Sqrt((p1.X - p2.X) * (p1.X - p2.X) + (p1.Y - p2.Y) * (p1.Y - p2.Y));
+        }
     }
 }
