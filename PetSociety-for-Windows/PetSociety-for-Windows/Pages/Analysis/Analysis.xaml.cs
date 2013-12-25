718491a036bab80620b1c03ba51f99b5f6f7e38b
MoistyMoist <mr.lawrance@outlook.com>
2013-12-25 11:52:24 +0800

    Heatmap ALGORIThIM DONE!!!! HAHAHAHA HAHAHAHAHAHAHAHAHAHAHAH

--- PetSociety-for-Windows/PetSociety-for-Windows/Pages/Analysis/Analysis.xaml.cs
+++ PetSociety-for-Windows/PetSociety-for-Windows/Pages/Analysis/Analysis.xaml.cs
@@ -232,7 +232,7 @@ namespace PetSociety_for_Windows.Pages.Analysis
                 p.Template = this.Resources["HeatMap"] as ControlTemplate;
                 LostHeatLayer.Children.Add(p);
             }
-            mainMap.Children.Add(LostHeatLayer);
+           // mainMap.Children.Add(LostHeatLayer);
 
 
 
@@ -250,9 +250,10 @@ namespace PetSociety_for_Windows.Pages.Analysis
 
         private void ReDrawHeatMap(object sender, MapEventArgs e)
         {
+            mainMap.Children.Remove(c);
             Converter conveter = new Converter(mainMap.ZoomLevel);
             c = new Canvas { Width = mainMap.ViewportSize.Width, Height = mainMap.ViewportSize.Height };
-
+            
             for (int i = 0; i < StaticObjects.MapLosts.Count; i++)
             {
                 Ellipse pin = new Ellipse();
@@ -267,35 +268,55 @@ namespace PetSociety_for_Windows.Pages.Analysis
                 margin.Top = point.Y;
                 pin.Margin = margin;
 
-                pin.Height = 40;
-                pin.Width = 40;
+                pin.Height = 50;
+                pin.Width = 50;
 
-                LinearGradientBrush gradient = new LinearGradientBrush();
-                gradient.StartPoint = new Point(0, 0);
-                gradient.EndPoint = new Point(1, 1);
+
+                RadialGradientBrush gradient = new RadialGradientBrush();
+                gradient.GradientOrigin = new Point(0.5, 0.5);
+                gradient.RadiusX = 0.5;
+                gradient.RadiusY = 0.5;
+                gradient.Opacity = 1;
 
                 GradientStop color1 = new GradientStop();
-                color1.Color = Colors.Yellow;
-                color1.Offset = 0.2;
+                color1.Color = Color.FromArgb(255,255,0,0);
+                color1.Offset = 0;
                 gradient.GradientStops.Add(color1);
 
                 GradientStop color2 = new GradientStop();
-                color2.Color = Colors.Orange;
-                color2.Offset = 0.5;
+                color2.Color = Color.FromArgb(125, 204, 0, 0);
+                color2.Offset = 0.25;
                 gradient.GradientStops.Add(color2);
 
                 GradientStop color3 = new GradientStop();
-                color3.Color = Colors.Red;
-                color3.Offset = 0.8;
+                color3.Color = Color.FromArgb(75, 150, 0, 0);
+                color3.Offset = 0.50;
                 gradient.GradientStops.Add(color3);
+
+                GradientStop color4 = new GradientStop();
+                color4.Color = Color.FromArgb(25, 255, 255, 0);
+                color4.Offset = 0.75;
+                gradient.GradientStops.Add(color4);
+
+                GradientStop color5 = new GradientStop();
+                color5.Color = Color.FromArgb(5, 102, 178, 255);
+                color5.Offset = 0.9;
+                gradient.GradientStops.Add(color5);
 
                 pin.Fill = gradient;
 
                 
                 c.Children.Add(pin);
-                MapLayer layer = new MapLayer();
+
+
 
             }
+            for (int i = 0; i < canvas.Children.Count; i++)
+            {
+                var pix = canvas.Children;
+                
+            }
+            
             mainMap.Children.Add(c);
         }
        
