2d6ea4bf205aa501f4020c213eca74a0080d8d0f
MoistyMoist <mr.lawrance@outlook.coml.com>
2014-01-22 16:28:12 +0800

    dsadas

--- PetSociety-for-Windows/PetSociety-for-Windows/Pages/Others/SearchAddress.xaml.cs
+++ PetSociety-for-Windows/PetSociety-for-Windows/Pages/Others/SearchAddress.xaml.cs
@@ -15,6 +15,36 @@ namespace PetSociety_for_Windows.Pages.Others
         public SearchAddress()
         {
             InitializeComponent();
+
         }
+
+
+        private void SearchTB_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (e.Key == System.Windows.Input.Key.Enter)
+            {
+                WebClient loginRequest = new WebClient();
+                loginRequest.DownloadStringCompleted += new DownloadStringCompletedEventHandler(RetrieveAddressComplete);
+                loginRequest.DownloadStringAsync(new System.Uri("http://www.onemap.sg/API/services.svc/basicSearch?token=qo/s2TnSUmfLz+32CvLC4RMVkzEFYjxqyti1KhByvEacEdMWBpCuSSQ+IFRT84QjGPBCuz/cBom8PfSm3GjEsGc8PkdEEOEr&searchVal=" +SearchTB.Text.ToString()+ "&otptFlds=SEARCHVAL,CATEGORY&returnGeom=0&rset=1"));
+            }
+        }
+        protected void RetrieveAddressComplete(object sender, DownloadStringCompletedEventArgs e)
+        {
+            MessageBox.Show(e.Result.ToString());
+        }
+        protected void displayResult()
+        {
+            
+        }
+
+        protected void PlotOnMainMap()
+        {
+            
+        }
+
+        
+
+
+
     }
 }
